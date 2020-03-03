ad70ce797f5f89df9f37d49b21ac46fbbbea4665
Armoteo <armoteo@ukr.net>
2020-03-03 10:55:06 +0200

    first

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './App.module.scss';
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div className={style.App}>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
